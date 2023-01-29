143353f8366b929a7761117e112ba8849fa9e889
abdogareb <abdogareb@gmail.com>
2023-01-29 21:29:18 +0200

    second submission

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -3,5 +3,5 @@
 
 #esta5dem envieroment variables
 # momken ne5aleeh hard coded bardo
-aws s3 cp --recursive --acl public-read ./ s3://$S3_BUCKET/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./index.html s3://$S3_BUCKET/
+aws s3 cp --recursive --acl public-read ./www s3://$S3_BUCKET/
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://$S3_BUCKET/
